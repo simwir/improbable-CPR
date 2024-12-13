24b2f857e3c4d115bf6cab7af60fbfc03b887242
Simon Virenfeldt <4235153+simwir@users.noreply.github.com>
2024-12-13 21:17:35 +0100

    Remove vector from __init__.py as it was part of the template

--- improbable_cpr/__init__.py
+++ improbable_cpr/__init__.py
@@ -4,8 +4,7 @@ __init__ file.
 
 from .cpr import Cpr
 from .cpr_builder import CprBuilder
-from .vector import Vector2D
 from .version import __version__
 
 
-__all__ = ["Vector2D", "__version__", "Cpr", "CprBuilder"]
+__all__ = ["__version__", "Cpr", "CprBuilder"]
